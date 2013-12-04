88d6484a21bdd60705db0a7de25fc65aaebdcbbc
Daniel Collins <d880c172@ku.edu>
2013-12-03 21:50:04 -0600

    Added logging into twitter when the checkbox is checked

--- src/com/example/socialnexus/RegisterActivity.java
+++ src/com/example/socialnexus/RegisterActivity.java
@@ -1,16 +1,44 @@
 package com.example.socialnexus;
 
-import android.os.Bundle;
+
+
+
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+
 
 public class RegisterActivity extends Activity {
+	
+	private CheckBox twitterBox;
+	/** Twitter4j object */
+	private Twitter mTwitter;
+	/** The request token signifies the unique ID of the request you are sending to twitter  */
+	private RequestToken mReqToken;
+	/** The url that Twitter will redirect to after a user log's in - this will be picked up by your app manifest and redirected into this activity */
+	public static final String CALLBACK_URL = "social-nexus-android:///";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.register);
+		
+		mTwitter = new TwitterFactory().getInstance();
+		mTwitter.setOAuthConsumer(TwitterInfo.CONSUMER_KEY, TwitterInfo.CONSUMER_SECRET);
+		twitterBox = (CheckBox) findViewById(R.id.TwitterBox);
 	}
 
 	@Override
@@ -21,7 +49,81 @@ public class RegisterActivity extends Activity {
 	}
 
 	public void next(View view)
-	{
+	{		
+		if (twitterBox.isChecked())
+		{
+			loginTwitter();
+		}
+	}
+	
+	
+	private void loginTwitter() {
+		try 
+		{			
+			mReqToken = mTwitter.getOAuthRequestToken(CALLBACK_URL);
+			
+			WebView twitterSite = new WebView(this);
+			twitterSite.loadUrl(mReqToken.getAuthenticationURL());
+			setContentView(twitterSite);
+
+		} catch (TwitterException e) {
+			Toast.makeText(this, "error: " + e, Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	/**
+	 * Catch when Twitter redirects back to our {@link CALLBACK_URL}</br> 
+	 * We use onNewIntent as in our manifest we have singleInstance="true" if we did not the
+	 * getOAuthAccessToken() call would fail
+	 */
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);		
+		dealWithTwitterResponse(intent);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	
+	/**
+	 * Twitter has sent us back into our app</br> 
+	 * Within the intent it set back we have a 'key' we can use to authenticate the user
+	 * 
+	 * @param intent
+	 */
+	private void dealWithTwitterResponse(Intent intent) {
+		Uri uri = intent.getData();
+		if (uri != null && uri.toString().startsWith(CALLBACK_URL)) { // If the user has just logged in
+			String oauthVerifier = uri.getQueryParameter("oauth_verifier");
+
+			authoriseNewUser(oauthVerifier);
+		}
 		
+	}
+	
+	/**
+	 * Create an access token for this new user</br> 
+	 * Fill out the Twitter4j helper</br> 
+	 * And save these credentials so we can log the user straight in next time
+	 * 
+	 * @param oauthVerifier
+	 */
+	private void authoriseNewUser(String oauthVerifier) {
+		try {
+			AccessToken at = mTwitter.getOAuthAccessToken(mReqToken, oauthVerifier);
+			mTwitter.setOAuthAccessToken(at);
+			
+			//stores access Token and Secret into Twitter Info for later use
+			TwitterInfo.twitterAccessToken = at.getToken();
+			TwitterInfo.twitterAccessSecret = at.getTokenSecret();
+			
+			// Set the content view back after we changed to a webview
+			setContentView(R.layout.main);
+			
+		} catch (TwitterException e) {
+			Toast.makeText(this, "Twitter auth error x01, try again later", Toast.LENGTH_SHORT).show();
+		}
 	}
 }
---
+++ src/com/example/socialnexus/TwitterInfo.java
@@ -0,0 +1,30 @@
+package com.example.socialnexus;
+
+import android.content.SharedPreferences;
+import twitter4j.*;
+
+
+public class TwitterInfo {	
+	/** Name to store the users access token */
+	public static final String PREF_ACCESS_TOKEN = "accessToken";
+	/** Name to store the users access token secret */
+	public static final String PREF_ACCESS_TOKEN_SECRET = "accessTokenSecret";
+	/** Consumer Key generated when you registered your app at https://dev.twitter.com/apps/ */
+	public static final String CONSUMER_KEY = "h62uYdnyVRfCw7fgi1SRA";
+	/** Consumer Secret generated when you registered your app at https://dev.twitter.com/apps/  */
+	public static final String CONSUMER_SECRET = "TNud0gKhwJIgblVJ04h4Pe1Gj8AkhwHW2Fi32om5c"; // XXX Encode in your app
+	/** The url that Twitter will redirect to after a user log's in - this will be picked up by your app manifest and redirected into this activity */
+	//public static final String CALLBACK_URL = "social-nexus-android:///";
+	/** Preferences to store a logged in users credentials */
+	public static SharedPreferences mPrefs;
+	
+	public static String twitterAccessToken = "";
+	public static String twitterAccessSecret = "";
+	
+	/** Twitter4j object */
+	//public static Twitter mTwitter;
+	/** The request token signifies the unique ID of the request you are sending to twitter  */
+	//public static RequestToken mReqToken;
+	
+
+}

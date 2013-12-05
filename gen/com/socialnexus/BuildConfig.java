0cee986c2ab020aa9d8a2fc29f26c51ff558dfd6
Greg Ervin <KaosuRyoko@gmail.com>
2013-12-04 19:46:37 -0600

    Final Commit on behalf of Leon Wu.

--- gen/com/socialnexus/BuildConfig.java
+++ gen/com/socialnexus/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.socialnexus;
+package com.socialnexus;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
